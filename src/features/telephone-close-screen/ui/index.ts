07f326ab6247513bca618d2a423667afa26b3317
Егор <124521323+f1k13@users.noreply.github.com>
2023-10-21 22:59:27 +0300

    feat: add button close telephone

---
+++ src/features/telephone-close-screen/ui/index.ts
@@ -0,0 +1,1 @@
+export { default as TelephoneCloseScreen } from "./telephone-close-screen";
--- src/pages/telephone-page/telephone-page.tsx
+++ src/pages/telephone-page/telephone-page.tsx
@@ -1,9 +1,13 @@
+import { TelephoneCloseScreen } from "@/features/telephone-close-screen/ui";
 import { Telephone } from "@/widgets/telephone/ui";
 
 const TelephonePage = () => {
   return (
-    <div className="bg-telephone bg-auto bg-no-repeat w-full h-full">
+    <div className="bg-telephone relative bg-auto flex bg-no-repeat w-full h-full">
       <Telephone />
+      <div className="absolute left-[61%] top-[20px] ">
+        <TelephoneCloseScreen />
+      </div>
     </div>
   );
 };
